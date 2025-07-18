52ed504b69da10e26a818c62395bcaac345c866a
Rami Hatoum <ramihatoum00@gmail.com>
2025-07-18 07:43:24 +0000

    enhance?

--- src/components/modals/surgical-briefing-modal.tsx
+++ src/components/modals/surgical-briefing-modal.tsx
@@ -1,3 +1,4 @@
+
 import { useState, useMemo } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
